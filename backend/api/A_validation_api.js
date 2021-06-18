c5773e815eaa895f031aee257fd83211eb5b5fda
Caio Leandro <caioleandro38@gmail.com>
2021-06-18 00:38:59 -0300

    Criação da API de validação de usuário

---
+++ backend/api/A_validation_api.js
@@ -0,0 +1,38 @@
+module.exports = app => {
+
+    const bcrypt = require('bcrypt')
+
+    const validation = async (req, res) => {
+        try {
+
+            const body = req.body
+
+            const user = await app.knex('tbl_users').where({ username: body.username }).select('username', 'senha').first()
+
+            const password = bcrypt.compareSync(body.senha, user.senha)
+
+            if(user.username == body.username && password){
+                console.log("Validado")
+
+                res.status(200).send({ message: "Usuário validado!", data: user })             
+            }
+            console.log("Não validado")
+            
+            return res.status(400).send({ message: "Usuário não validado!" })
+
+        } catch (error) {
+            console.log(error)
+
+            return res.status(500).send({ message: "Ocorreu um erro!" })
+        }
+    }
+
+    // async function return_id (body) {
+
+    //     const id = await app.knex('tbl_users').where({ username: body.username }).select('id_user').first()
+
+    //     return id.id_user 
+    // }
+
+    return { validation }
+}
