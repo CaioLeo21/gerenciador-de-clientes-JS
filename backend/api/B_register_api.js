a74d9e4c88a183fdefa69f2232bd323c11a29318
Caio Leandro <caioleandro38@gmail.com>
2021-06-18 00:39:14 -0300

    Criação da API de registro de cliente

---
+++ backend/api/B_register_api.js
@@ -0,0 +1,24 @@
+module.exports = app => {
+
+    //const return_id = app.api.A_validation_api.validation
+
+    const register = async (req, res) => {
+        try {
+
+            const body = req.body
+
+            await app.knex('tbl_clientes').insert(body, {
+                id_user: 1
+            })
+
+            return res.status(201).send({ message: "Cliente criado!", data: body })
+
+        } catch (error) {
+            console.log(error)
+            
+            return res.status(500).send({ message: "Cliente não cadastrado" })
+        }
+    }
+
+    return { register }
+}
